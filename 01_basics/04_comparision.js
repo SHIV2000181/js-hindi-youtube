ceb1772a9f42894bc0807ee6b1dbc9a7229081c3
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-17 15:27:24 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);  //these type of good
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);  //these type are bad 
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);   //these type are bad 
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);     //these type are bad
+
+
+//  ===
+
+console.log("2" === 2);
+
+
+
+
+
+
