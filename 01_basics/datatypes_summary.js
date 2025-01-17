7504ae3106e7632b8a99fbd1339baf29c70c2bda
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-17 17:06:34 +0000

    memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -39,4 +39,36 @@ const myFunction =function(){
 console.log(typeof bigNumber);
 
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// https://262.ecma-international.org/5.1/#sec-11.4.3    
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//memory types
+// Stack (primitive), Heap (Non-primitive)
+
+let myYoutubename = "hiteshchaudhrydotcom"
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+
+
+// Heap 
+let userOne = {
+    email: "user@goole.com",
+    upi: "userOne"
+}
+
+let userTwo = userOne
+
+console.log(userTwo);
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
