672f030a0cd0f1260ae216f27efad75f9c02413c
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-17 13:37:30 +0000

    comit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+// mdn documentation & (tc39.es)ecma   //for reding side
+
+"use strict"  //treat all Js code an newwer version
+
+//  alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+console.log("Hitesh")
+
+
+let mane = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+
+// console.log(typeof"hitesh");
+// console.log(typeof age);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefind
+
+
+
