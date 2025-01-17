daaa7d7c67c268636a58c91349ca9f5148d20775
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-17 13:02:54 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "shivsagar@123"
+var accountPassword = "12344"
+accountCity = "jaipur";
+let accountState;
+
+// accountId = 2 //not allowed
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountState]);
