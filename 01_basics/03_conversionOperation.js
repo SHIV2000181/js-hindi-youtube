52bf5b4bc9245248ce776e7b32bada78ec45fdde
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-17 14:02:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"  //NaN
+// let score = null  // 0
+// let score = undefined //NaN
+// let score = true //1
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"  => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "shiv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false;
+//"" => false
+//"shiv" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
