2207a348ef3a1be587ae3075f416762829b7c23c
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-20 14:42:46 +0000

    forEach & filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,42 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+
+// console.log(values);    //ye return kun nhi kar rha hai (undefined)
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => num > 4 ) //{} nhi lagate hai to return key word nhi likhte hai
+
+// const newNums = myNums.filter( (num) => {    //{} layege to return key word likhana padta hai 
+//     return num > 4;                         //return nhi likhege to data access nhi hoga  
+// })
+// console.log(newNums);
+
+
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+
+// })
+
+
+// console.log(newNums);
+
+
+const 
+
+
+
+
+
