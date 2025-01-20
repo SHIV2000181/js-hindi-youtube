3cb39131cd2caa1a8285108685b9baa3ac8c9217
Shiv sagar yadav <ssy312002@gmail.com>
2025-01-20 16:20:18 +0000

    forEach & filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -34,7 +34,28 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(newNums);
 
 
-const 
+const books = [
+    {title: 'book One', genre: 'Fiction', publish: 1981, edition: 2004},
+    {title: 'book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+    {title: 'book Three', genre: 'History', publish: 1999, edition: 2007},
+    {title: 'book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010},
+    {title: 'book Five', genre: 'Science', publish: 2009, edition: 2014},
+    {title: 'book Six', genre: 'Fiction', publish: 1987, edition: 2010},
+    {title: 'book Seven', genre: 'History', publish: 1986, edition: 1996},
+    {title: 'book Eight', genre: 'Science', publish: 2011, edition: 2016},
+
+];
+
+
+let userBooks = books.filter( (bk) => bk.genre === 'History');
+
+userBooks = books.filter( (bk) => {return bk.publish >= 1995} )
+
+userBooks = books.filter( (bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+
+console.log(userBooks);
 
 
 
